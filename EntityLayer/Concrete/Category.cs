0ea63f8439ad74d4bfac68f442cd35fafeff3ad8
erenkizilyer <erenkizilyer662@gmail.com>
2024-01-02 00:48:34 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [StringLength(200)]
+        public string CategoryDescription { get; set; }
+
+
+        public bool CategoryStatus { get; set; }
+
+
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ContentID { get; set; }
+
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int? WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+    }
+}
